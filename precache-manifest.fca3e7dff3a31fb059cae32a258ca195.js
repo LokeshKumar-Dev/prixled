e2dddcbc240e3713be33eac24c811261db2c64ac
lokesh <adobep40@gmail.com>
2020-10-14 19:04:11 -0700

    Updates

---
+++ precache-manifest.fca3e7dff3a31fb059cae32a258ca195.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7c79930ca658f59f5fb810152a8c067",
+    "url": "/prixled/index.html"
+  },
+  {
+    "revision": "b8ec0d80d6dd4f531874",
+    "url": "/prixled/static/css/main.3e475dcc.chunk.css"
+  },
+  {
+    "revision": "5949ef739446db8e5215",
+    "url": "/prixled/static/js/2.577b8a90.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/prixled/static/js/2.577b8a90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8ec0d80d6dd4f531874",
+    "url": "/prixled/static/js/main.e791a3a3.chunk.js"
+  },
+  {
+    "revision": "32ce3bf39d567e7e95fd",
+    "url": "/prixled/static/js/runtime-main.177a776c.js"
+  },
+  {
+    "revision": "0c05c13fa22112c56d67bae77ff948e9",
+    "url": "/prixled/static/media/Comingsoonbackground.0c05c13f.png"
+  },
+  {
+    "revision": "2ba58ba3a1ece25985475bd32fe3a0dd",
+    "url": "/prixled/static/media/Logo.2ba58ba3.svg"
+  },
+  {
+    "revision": "92a642223f301d4e65ccced732a30052",
+    "url": "/prixled/static/media/bg-anime.92a64222.svg"
+  }
+]);
